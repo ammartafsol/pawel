58eadda8db9642863ebc7f8e09ff6999bb20b0ed
ammartafsol <ammar.js@tafsol.com>
2025-11-24 13:42:02 +0500

    updated

--- src/components/Template/Home/index.jsx
+++ src/components/Template/Home/index.jsx
@@ -3,7 +3,7 @@ import styles from "./HomeTemplate.module.css";
 
 export default function HomeTemplate() {
   return (
-    <main className={styles.main}>
+    <main className={`fs-28`}>
       Example Project
       <Link href="/components">See Components</Link>
     </main>
