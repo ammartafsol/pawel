aa1a82f49f585008106276f8b52d6d0fec61b3f2
ammartafsol <ammar.js@tafsol.com>
2025-11-24 15:25:15 +0500

    Enhance LoginTemplate with input fields, checkbox, and button; add corresponding styles for layout and design.

--- src/components/Template/Auth/LoginTemplate/LoginTemplate.jsx
+++ src/components/Template/Auth/LoginTemplate/LoginTemplate.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
-import classes from "./LoginTemplate.module.css"
-import AuthWrapper from '@/components/atoms/AuthWrapper/AuthWrapper';
+import React from "react";
+import classes from "./LoginTemplate.module.css";
+import AuthWrapper from "@/components/atoms/AuthWrapper/AuthWrapper";
+import Input from "@/components/atoms/Input/Input";
+import Button from "@/components/atoms/Button";
+import Checkbox from "@/components/atoms/Checkbox";
 
 const LoginTemplate = () => {
   return (
-    <AuthWrapper title="Login to your account" description="Enter your details to login">
-      <div>ew</div>
+    <AuthWrapper
+      title="Login to your account"
+      description="Enter your details to login"
+    >
+      <div className={classes.loginForm}>
+        <div className={classes.inputContainer}>
+          <Input type="email" label="Email" placeholder="Enter your email" />
+          <Input label="Password" placeholder="Enter your password" />
+        <div className={classes.buttonContainer}> 
+          <div className={classes.forgotPasswordContainer}>
+            <Checkbox label="Keep me logged in" />
+            <div className={classes.forgotPassword}>Forgot password?</div>
+          </div>
+          <Button
+            variant="primary"
+            label="Sign In"
+            className={classes.loginButton}
+          />
+        </div>
+        </div>
+      </div>
     </AuthWrapper>
-  )
-}
+  );
+};
 
-export default LoginTemplate
+export default LoginTemplate;
