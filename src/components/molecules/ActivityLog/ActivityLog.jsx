a2de827cff2606fcdfcf9f0df746dda3c28ea66e
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-11-25 19:16:10 +0500

    activity

--- src/components/molecules/ActivityLog/ActivityLog.jsx
+++ src/components/molecules/ActivityLog/ActivityLog.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Activity from '../../atoms/Activity/Activity'
 import classes from './ActivityLog.module.css'
 
-export default function ActivityLog() {
+const DEFAULT_ACTIVITIES = [
+  { text: "Status update to Defense", date: "May 1, 2025" },
+  { text: "Status update to Evidence Round Opponent", date: "May 15, 2025" },
+  { text: "Document Upload", date: "May 15, 2025" }
+]
+
+export default function ActivityLog({ activities = DEFAULT_ACTIVITIES }) {
   return (
-    <div>
-      
+    <div className={classes.activityLogContainer}>
+      {activities.map((activity, index) => (
+        <Activity
+          key={index}
+          text={activity.text}
+          date={activity.date}
+          isLast={index === activities.length - 1}
+        />
+      ))}
     </div>
   )
 }
