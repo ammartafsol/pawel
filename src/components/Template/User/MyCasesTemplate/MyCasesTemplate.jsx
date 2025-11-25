ce82cfd3e8e2bb1f26146664a20b4c3a79002064
ammartafsol <ammar.js@tafsol.com>
2025-11-25 19:03:15 +0500

    updated

--- src/components/Template/User/MyCasesTemplate/MyCasesTemplate.jsx
+++ src/components/Template/User/MyCasesTemplate/MyCasesTemplate.jsx
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import classes from "./MyCasesTemplate.module.css";
 import Breadcrumbs from "@/components/molecules/Breadcrumbs/Breadcrumbs";
 import BreadComTop from "@/components/atoms/BreadComTop/BreadComTop";
-import { caseProgressCardsData, statesCaseData } from "@/developementContent/Data/dummtData/dummyData";
+import {
+  caseProgressCardsData,
+  statesCaseData,
+} from "@/developementContent/Data/dummtData/dummyData";
 import { gridFilter } from "@/developementContent/Enums/enum";
 import GridFilter from "@/components/molecules/GridFilter/GridFilter";
 import { Col, Row } from "react-bootstrap";
 import CaseProgressCard from "@/components/molecules/CaseProgressCard/CaseProgressCard";
 import Wrapper from "@/components/atoms/Wrapper/Wrapper";
+import ResponsiveTable from "@/components/organisms/ResponsiveTable/ResponsiveTable";
 
 const MyCasesTemplate = () => {
   const [activeGridFilter, setActiveGridFilter] = useState(gridFilter[0]);
@@ -22,32 +26,36 @@ const MyCasesTemplate = () => {
         activeGridFilter={activeGridFilter}
         setActiveGridFilter={setActiveGridFilter}
       />
-      {
-        activeGridFilter.value === "table" ? (
-          <div>
-            <Wrapper title="My Cases" searchValue={searchValue} setValue={setSearchValue} searchPlaceholder="Search">
-
-            </Wrapper>
-          </div>
-        ) : (
-          <Row className="g-4">
-          {
-            caseProgressCardsData.map((item) => (
-              <Col className="col-12 col-md-4" key={item.id}>
-                <CaseProgressCard data={{
+      {activeGridFilter.value === "table" ? (
+        <div>
+          <Wrapper
+            title="My Cases"
+            searchValue={searchValue}
+            setValue={setSearchValue}
+            searchPlaceholder="Search"
+          >
+            <ResponsiveTable />
+          </Wrapper>
+        </div>
+      ) : (
+        <Row className="g-4">
+          {caseProgressCardsData.map((item) => (
+            <Col className="col-12 col-md-4" key={item.id}>
+              <CaseProgressCard
+                data={{
                   tabLabel: item.tabLabel,
                   userName: item.userName,
                   progress: item.progress,
                   status: item.status,
                   trademarkName: item.trademarkName,
                   trademarkNo: item.trademarkNo,
-                  referenceLink: item.referenceLink
-                }} />
-              </Col>
-            ))
-          }
-        </Row>        )
-      }
+                  referenceLink: item.referenceLink,
+                }}
+              />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
