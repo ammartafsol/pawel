87993dfa679eb4c9ba9fb9e006c95cb144907538
ammartafsol <ammar.js@tafsol.com>
2025-11-25 17:37:25 +0500

    updated

---
+++ src/components/molecules/Breadcrumbs/Breadcrumbs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from "./Breadcrumbs.module.css"
+
+const Breadcrumbs = () => {
+  return (
+    <div>Breadcrumbs</div>
+  )
+}
+
+export default Breadcrumbs
---
+++ src/developementContent/Data/dummtData/dummyData.js
@@ -0,0 +1,29 @@
+export const caseProgressCardsData = [
+    {
+        id: 1,
+        tabLabel: "EU TM OPPO",
+        userName: "Darlene Steuber",
+        progress: 80,
+        status: "Decision",
+        trademarkName: "Crist and Sons",
+        trademarkNo: "R-3526",
+        referenceLink: "#"
+    },
+    {
+        id: 2,
+        tabLabel: "EU TM OPPO",
+        userName: "Darlene Steuber",
+        progress: 90,
+        status: "Decision",
+        trademarkName: "Crist and Sons",
+        trademarkNo: "R-3526",
+        referenceLink: "#"
+    },
+]
+
+
+export const circularCaseProgressChartData = [
+    { label: 'In progress', value: 2, color: '#8E5AFD' },
+    { label: 'Completed', value: 1, color: '#33C8C2' },
+    { label: 'Not Started', value: 0.5, color: '#B7EAE8' }
+  ]
