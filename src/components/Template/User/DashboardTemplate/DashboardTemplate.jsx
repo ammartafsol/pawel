06b7c952c560d02508b3eb477f2031f4c6d1c1e3
ammartafsol <ammar.js@tafsol.com>
2025-11-25 16:46:47 +0500

    Refactor DashboardTemplate to include Bootstrap layout; add date display and greeting for user. Comment out Calendar and ResponsiveTable components for future implementation.

--- src/components/Template/User/DashboardTemplate/DashboardTemplate.jsx
+++ src/components/Template/User/DashboardTemplate/DashboardTemplate.jsx
@@ -1,10 +1,12 @@
 "use client";
-import React from 'react';
-import Wrapper from '@/components/atoms/Wrapper/Wrapper';
-import Calender from '@/components/molecules/Calender/Calender';
-import ResponsiveTable from '@/components/organisms/ResponsiveTable/ResponsiveTable';
-import ModalSkeleton from '@/components/organisms/Modals/ModalSkeleton/ModalSkeleton';
-import Button from '@/components/atoms/Button';
+import React from "react";
+import classes from "./DashboardTemplate.module.css";
+import Wrapper from "@/components/atoms/Wrapper/Wrapper";
+import Calender from "@/components/molecules/Calender/Calender";
+import ResponsiveTable from "@/components/organisms/ResponsiveTable/ResponsiveTable";
+import ModalSkeleton from "@/components/organisms/Modals/ModalSkeleton/ModalSkeleton";
+import Button from "@/components/atoms/Button";
+import { Col, Row } from "react-bootstrap";
 
 const DashboardTemplate = () => {
   // Sample events data - events shown in time slots
@@ -33,28 +35,48 @@ const DashboardTemplate = () => {
 
   return (
     <div>
-      <Wrapper title="Recent Case Statuses">
-        {/* ////calender     */}
-        <Calender events={myEventsList} />
-      </Wrapper>
-        <ResponsiveTable 
-        tableHeader={[{
-          title: "Case ID",
-          key: "caseId",
-        },{
-          title: "Case Status",
-          key: "caseStatus",
-        }]}
-        data={[{
-          caseId: "123456",
-          caseStatus: "Open",
-        },{
-          caseId: "123456",
-          caseStatus: "Open",
-        }]}
-        />
+      <Row>
+        <Col className="col-12 col-md-8">
+        <div className={classes.leftTop}>
+          <div className={classes.dateText}>
+            {new Date().toLocaleDateString("en-US", {
+              weekday: "long",
+              day: "numeric",
+              month: "long",
+              year: "numeric",
+            })}
+          </div>
+          <h4>Good evening, Joe.</h4>
+        </div>
+        </Col>
+        <Col className="col-12 col-md-4">
+        2
+        </Col>
+      </Row>
     </div>
   );
 };
 
 export default DashboardTemplate;
+
+{
+  /* <Calender events={myEventsList} /> */
+}
+{
+  /* <ResponsiveTable 
+tableHeader={[{
+  title: "Case ID",
+  key: "caseId",
+},{
+  title: "Case Status",
+  key: "caseStatus",
+}]}
+data={[{
+  caseId: "123456",
+  caseStatus: "Open",
+},{
+  caseId: "123456",
+  caseStatus: "Open",
+}]}
+/> */
+}
