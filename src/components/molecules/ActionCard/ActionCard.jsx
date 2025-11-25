6dd7e8ad1bb38c3f3aa24dbf6a4f8f0a20fcd458
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-11-25 18:36:35 +0500

    action card in progress

--- src/components/molecules/ActionCard/ActionCard.jsx
+++ src/components/molecules/ActionCard/ActionCard.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 import classes from './ActionCard.module.css'
 
-export default function ActionCard() {
+export default function ActionCard({ image, text, onClick }) {
   return (
-    <div>
-      
+    <div className={classes.card} onClick={onClick}>
+      <div className={classes.iconContainer}>
+        <Image
+          src={`/app-images/${image}.png`}
+          alt={text}
+          width={48}
+          height={48}
+          className={classes.icon}
+        />
+      </div>
+      <span className={classes.text}>{text}</span>
     </div>
   )
 }
