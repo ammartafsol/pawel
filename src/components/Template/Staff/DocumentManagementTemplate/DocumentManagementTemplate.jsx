1d15f8d2b7087f875be84a564265e6827371d56d
ammartafsol <ammar.js@tafsol.com>
2025-11-27 14:43:17 +0500

    Enhance DocumentManagementTemplate with new grid filter functionality, improved layout for document display, and updated styles for better visual presentation.

--- src/components/Template/Staff/DocumentManagementTemplate/DocumentManagementTemplate.jsx
+++ src/components/Template/Staff/DocumentManagementTemplate/DocumentManagementTemplate.jsx
@@ -1,20 +1,130 @@
-"use client"
-
+"use client";
+import React, { useState } from "react";
+import classes from "./DocumentManagementTemplate.module.css";
 import Wrapper from "@/components/atoms/Wrapper/Wrapper";
-import classes from "./DocumentManagementTemplate.module.css"
+import TableHeader from "@/components/molecules/TableHeader/TableHeader";
+import AppTable from "@/components/organisms/AppTable/AppTable";
+import GridFilter from "@/components/molecules/GridFilter/GridFilter";
+import { gridFilter } from "@/developementContent/Enums/enum";
+import { documentManagementTableHeader } from "@/developementContent/TableHeader/DocumentManagementTableHeader";
+import { documentManagementTableBody } from "@/developementContent/TableBody/DocumentManagementTableBody";
+import { GoQuestion } from "react-icons/go";
+import { Col, Row } from "react-bootstrap";
 
 const DocumentManagementTemplate = () => {
+  const [activeGridFilter, setActiveGridFilter] = useState(gridFilter[0]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleFilterClick = () => {
+    console.log("Filter clicked");
+  };
+
+  const filterOptions = [
+    {
+      label: "All",
+      onClick: () => {
+        console.log("Filter: All");
+      }
+    },
+    {
+      label: "TM Opposition",
+      onClick: () => {
+        console.log("Filter: TM Opposition");
+      }
+    },
+    {
+      label: "Design invalidation",
+      onClick: () => {
+        console.log("Filter: Design invalidation");
+      }
+    },
+    {
+      label: "Privacy Violations",
+      onClick: () => {
+        console.log("Filter: Privacy Violations");
+      }
+    },
+    {
+      label: "Employment Disputes",
+      onClick: () => {
+        console.log("Filter: Employment Disputes");
+      }
+    }
+  ];
 
   return (
-   <div className="p24">
-    <Wrapper >
-      <div className={classes?.documentManagementContainer}></div>
-    </Wrapper>
-   </div>
-  )
-}
+    <div className="p24">
+      <GridFilter
+        classesName={classes.gridFilter}
+        gridFilter={gridFilter}
+        activeGridFilter={activeGridFilter}
+        setActiveGridFilter={setActiveGridFilter}
+      />
+      {activeGridFilter.value === "table" ? (
+        <Wrapper
+          headerComponent={
+            <TableHeader
+              title="Document Management"
+              titleIcon={<GoQuestion color="#D9D9D9" size={20} />}
+              searchValue={searchValue}
+              onSearchChange={setSearchValue}
+              searchPlaceholder="Search..."
+              onFilterClick={handleFilterClick}
+              filterOptions={filterOptions}
+              viewButtonText="Upload Document"
+              onClickViewAll={() => {
+                console.log("Upload Document clicked");
+              }}
+            />
+          }
+          contentClassName={classes.contentClassName}
+        >
+          <AppTable
+            tableHeader={documentManagementTableHeader}
+            data={documentManagementTableBody}
+          />
+        </Wrapper>
+      ) : (
+        <div>
+          <Wrapper
+            headerComponent={
+              <TableHeader
+                title="Document Management"
+                titleIcon={<GoQuestion color="#D9D9D9" size={20} />}
+                searchValue={searchValue}
+                onSearchChange={setSearchValue}
+                searchPlaceholder="Search..."
+                onFilterClick={handleFilterClick}
+                filterOptions={filterOptions}
+                viewButtonText="Upload Document"
+                onClickViewAll={() => {
+                  console.log("Upload Document clicked");
+                }}
+              />
+            }
+            contentClassName={classes.contentClassName}
+          >
+            <Row className="g-4">
+              {documentManagementTableBody.map((item) => (
+                <Col className="col-12 col-md-4" key={item.id}>
+                  <div className={classes.documentCard}>
+                    <h5>{item.documentName}</h5>
+                    <p><strong>Client:</strong> {item.clientName}</p>
+                    <p><strong>Type:</strong> {item.typeOfCase}</p>
+                    <p><strong>Trade Mark:</strong> {item.tradeMarkNo}</p>
+                    <p><strong>Date:</strong> {item.dateUploaded}</p>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </Wrapper>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default DocumentManagementTemplate
+export default DocumentManagementTemplate;
 
 
 
