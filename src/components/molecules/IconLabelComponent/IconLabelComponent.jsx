c3dfae7d395661697ce69eba63b0251f85d8ad0f
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-11-27 16:20:54 +0500

    pro

---
+++ src/components/molecules/IconLabelComponent/IconLabelComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './IconLabelComponent.module.css'
+
+export default function IconLabelComponent() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
